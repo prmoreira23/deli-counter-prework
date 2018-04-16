86c8937695b7a7738a2f263b5ce55ad6cc0507d8
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-16 04:00:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(katz_deli)
+    message = "The line is currently: "
+    lines = []
+    if katz_deli.size == 0
+        puts "The line is currently empty."
+    else
+        katz_deli.each_with_index do |item, index|
+            lines << "#{(index + 1)}. #{item}"
+        end
+        message += lines.join(" ")
+        puts message
+    end
+end
+
+def take_a_number(array, people)
+    array << people
+    puts "Welcome, #{people}. You are number #{array.size} in line." if people.class == String
+end
+
+def now_serving(line)
+    if line.size < 1
+        puts "There is nobody waiting to be served!"
+    else
+        person = line.shift
+        puts "Currently serving #{person}."
+    end
+end
